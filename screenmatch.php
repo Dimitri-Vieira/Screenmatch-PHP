e2b60841784dfd97391c08990c8f64d1cc667f58
Dimitri-Vieira <dimitripiriswork@gmail.com>
2025-02-24 18:28:51 -0300

    Fazendo upload dos arquivos do projeto (V.1)

---
+++ screenmatch.php
@@ -0,0 +1,52 @@
+<?php
+
+echo "Bem-vindo(a) ao ScreenMatch!\n";
+
+$nomeFilme = "Top Gun - Maverick";
+
+$anoLancamento = 2022;
+
+$quantidadeDeNotas = $argc -1;
+$notas = [];
+
+for ($contador = 1; $contador < $argc; $contador++){
+    // quando não é indicado o array do índice, por padrão, é selecionada a última posição
+    $notas[] = (float) $argv[$contador]; 
+}
+
+$notaFilme = array_sum($notas) / $quantidadeDeNotas;
+$planoPrime = true;
+
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nota do filme: $notaFilme\n";
+echo "Ano de lançamento> $anoLancamento\n";
+
+if ($anoLancamento > 2022){
+    echo "Esse filme é um lançamento\n";
+
+} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022){
+    echo "Esse filme ainda é novo\n";
+
+} else {
+    echo "Esse filme não é um lançamento\n";
+}
+
+$genero = match ($nomeFilme){
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "comédia",
+    "Se beber não case" => "comédia",
+    default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é $genero\n";
+
+$filme = [
+    "nome" => "Thor: Ragnarok",
+    "ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói",
+];
+
+echo $filme["ano"];
